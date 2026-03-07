c11f5a83f100398a7cd318369e7c7fb9c7c8aa4c
Ohmwrecker <2512008088@qq.com>
2026-03-07 15:38:53 +0800

    P5723

--- src/题单102_循环结构/P5723.cpp
+++ src/题单102_循环结构/P5723.cpp
@@ -1,34 +1,34 @@
-#include <iostream>
-
-// 爆WA是因为判断质数函数不正确
-bool isPrime(int n) {
-	if (n == 2 || n == 3 || n == 5 || n == 7) {
-		return true;
-	}
-	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0) {
-		return false;
-	}
-	else {
-		return true;
-	}
-}
-
-int main() {
-	int maxNum = 0, curNum = 0, count = 0;
-	std::cin >> maxNum;
-	int i = 2;
-	while (curNum <= maxNum) {
-		if (isPrime(i)) {
-			if ((curNum + i) <= maxNum) {
-				curNum += i;
-				std::cout << i << std::endl;
-				count++;
-			}
-			else {
-				break;
-			}
-		}
-		i++;
-	}
-	std::cout << count;
-}
+//#include <iostream>
+//
+//// 爆WA是因为判断质数函数不正确
+//bool isPrime(int n) {
+//	if (n == 2 || n == 3 || n == 5 || n == 7) {
+//		return true;
+//	}
+//	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0) {
+//		return false;
+//	}
+//	else {
+//		return true;
+//	}
+//}
+//
+//int main() {
+//	int maxNum = 0, curNum = 0, count = 0;
+//	std::cin >> maxNum;
+//	int i = 2;
+//	while (curNum <= maxNum) {
+//		if (isPrime(i)) {
+//			if ((curNum + i) <= maxNum) {
+//				curNum += i;
+//				std::cout << i << std::endl;
+//				count++;
+//			}
+//			else {
+//				break;
+//			}
+//		}
+//		i++;
+//	}
+//	std::cout << count;
+//}
